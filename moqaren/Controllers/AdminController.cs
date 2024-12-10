0f83ca18dda79ede4b8a29356e5b9fed18859b78
Alhasan Halawani <dr.hasun@msn.com>
2024-12-10 19:11:19 +0300

    debug page /admin

---
+++ moqaren/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿// Create a new file Controllers/AdminController.cs
+using Microsoft.AspNetCore.Mvc;
+
+namespace moqaren.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Debug()
+        {
+            return View();
+        }
+    }
+}
